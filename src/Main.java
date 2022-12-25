a94622b0929b4eff00e18e4d3346d2a073c9b0bf
ViktorK <nokksaxs@gmail.com>
2022-12-25 21:28:01 +0300

    added task 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+//27. Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
+// The relative order of the elements may be changed.
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        for(int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
     }
 }
